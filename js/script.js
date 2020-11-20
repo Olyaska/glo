ea164ba6adaf8208898b7d2dc03e93c1b9321d5e
Olga Shtareva <olga-shtar@ya.ru>
2020-11-20 13:45:47 +0300

    feat(script.js) add lesson_02 tasks

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 'use strict';
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
 
-alert("It's alert message!");
+// 1.
+const money = 40000,
+    income = 'фриланс',
+    deposit = true,
+    mission = 200000,
+    period = 10;
+let addExpenses = 'Учеба, коммуналка, еда';
 
-console.log('Hello from console');
+// 2.
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+addExpenses = addExpenses.toLowerCase();
+addExpenses = addExpenses.split(', ');
+console.log(addExpenses);
+
+const budgetDay = money / 30;
+console.log(budgetDay);
