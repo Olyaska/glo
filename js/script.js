14116c1eb15d9c377d22f0eb28740d854049c214
Olga Shtareva <olga-shtar@ya.ru>
2020-11-23 13:07:39 +0300

    feat(script.js) add lesson_03 tasks

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 'use strict';
 
-const money = 40000,
+const money = +prompt('Ваш месячный доход?'), //2.
     income = 'фриланс',
-    deposit = true,
-    mission = 200000,
+    deposit = confirm('Есть ли у вас депозит в банке?'), //4.
+    mission = 100000,
     period = 10,
-    addExpenses = 'Учеба, коммуналка, еда',
-    budgetDay = money / 30;
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую:'), //3.
+    expenses1 = prompt('Введите обязательную статью расходов:'), //5.
+    amount1 = +prompt('Во сколько это обойдется?'),
+    expenses2 = prompt('Введите вторую обязательную статью расходов:'),
+    amount2 = +prompt('Во сколько это обойдется?'),
+    budgetMonth = money - amount1 - amount2, //6.
+    achievementSpeed = Math.ceil(mission/budgetMonth), //7.
+    budgetDay = Math.floor(budgetMonth/30); //8.
 
 console.log(typeof money, typeof income, typeof deposit);
 
@@ -17,4 +23,12 @@ console.log('Цель заработать ' + mission + ' рублей');
 
 console.log(addExpenses.toLowerCase().split(', '));
 
-console.log(budgetDay);
+console.log('Цель будет достигнута за: ' + achievementSpeed + ' месяцев'); //7.
+console.log('Бюджет на месяц: ', budgetMonth); //6.
+console.log('Бюджет на день: ', budgetDay); //8.
+
+//9.
+if (budgetDay >= 1200) console.log('У вас высокий уровень дохода');
+else if (budgetDay >= 600 && budgetDay < 1200) console.log('У вас средний уровень дохода');
+else if (budgetDay < 600 && budgetDay >= 0) console.log('К сожалению, у вас уровень дохода ниже среднего');
+else console.log('Что-то пошло не так: Ваши расходы превышают Ваши доходы');
